9c99ef6e0da65cfb21384a432f120c8a37d35b82
iamrice <woshichentaiyou@qq.com>
2020-12-20 13:47:20 +0800

    添加页面跳转

--- web/src/index.js
+++ web/src/index.js
@@ -5,13 +5,14 @@ import './index.css';
 import SearchBlock from './indexPage/searchBlock';
 import Exhibiton from './indexPage/commodityExhibition';
 import Create from './indexPage/create';
+import CommodityDetails from './indexPage/commodityDetails'
 import * as serviceWorker from './serviceWorker';
 import {
     BrowserRouter as Router,
     Route,
     Link,
     Redirect
-} from 'react-router-dom'; 
+} from 'react-router-dom';
 import {
     Layout,
 } from 'antd';
@@ -42,12 +43,7 @@ class ContainBox extends React.Component {
         }
         this.requestData();
         this.allData = [];
-        this.props.history.push({
-            pathname: "/create",
-            state: {
-                id: 1
-            }
-        })
+
     }
     render() {
         console.log(this.state.windowWidth)
@@ -158,34 +154,15 @@ class ContainBox extends React.Component {
             }
         })
     }
-    createCom(data) {
-        var that = this
-        var postData = new FormData();
-        postData.append('context', data)
-        $.ajax({
-            url: that.url.local + "addCommodity/",
-            dataType: "json",
-            type: "POST",
-            async: false, //同步上传
-            cache: false, //上传文件无需缓存
-            processData: false, // 不处理数据
-            contentType: false, // 不设置内容类型
-            data: postData,
-            success: function(res) {
-                
-                that.successCreate();
-            }
-        })
-    }
 };
- 
+
 ReactDOM.render(
     <Router>
         <Route path="/">
             <Route path="/index" exact component={ContainBox}/>
-            <Route path="/search" component={SearchBlock}/>
             <Route path="/create" component={Create}/>
-            <Redirect path="/" to={{pathname:'/create'}} />
+            <Route path="/details" component={CommodityDetails}/>       
+            <Redirect path="/" to={{pathname:'/index'}} />
         </Route>
     </Router>,
     document.getElementById('container')
